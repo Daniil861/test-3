6401913fab2fb330e3b3e03e722b0ce8629ec41d
Daniil861 <90471703+Daniil861@users.noreply.github.com>
2022-02-11 13:30:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,63 +20,68 @@ const machine = new SlotMachine(el, {
 	auto: 1500
 });
 
-// Random Time
-var minTime = 500;
-var maxTime = 2000;
-function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
-}
+if (document.querySelector('.container')) {
 
-// Casiono game 2
-var casinoGold1 = document.querySelector('#name1');
-var casinoGold2 = document.querySelector('#name2');
-var casinoGold3 = document.querySelector('#name3');
-var casinoGold4 = document.querySelector('#name4');
-if (casinoGold1 && casinoGold2 && casinoGold3 && casinoGold4) {
-	var mcasinoGold1 = new SlotMachine(casinoGold1, {
-		active: 0,
-		delay: 500
-	});
-	var mcasinoGold2 = new SlotMachine(casinoGold2, {
-		active: 1,
-		delay: 500
-	});
-	var mcasinoGold3 = new SlotMachine(casinoGold3, {
-		active: 2,
-		delay: 500
-	});
-	var mcasinoGold4 = new SlotMachine(casinoGold4, {
-		active: 3,
-		delay: 500
-	});
-
-	function gameSlotTwo() {
-		mcasinoGold1.shuffle(9999);
-		mcasinoGold2.shuffle(9999);
-		mcasinoGold3.shuffle(9999);
-		mcasinoGold4.shuffle(9999);
-		setTimeout(() => mcasinoGold1.stop(), getRandomArbitrary(minTime, maxTime));
-		setTimeout(() => mcasinoGold2.stop(), getRandomArbitrary(minTime, maxTime));
-		setTimeout(() => mcasinoGold3.stop(), getRandomArbitrary(minTime, maxTime));
-		setTimeout(() => mcasinoGold4.stop(), getRandomArbitrary(minTime, maxTime));
+	// Random Time
+	var minTime = 500;
+	var maxTime = 2000;
+	function getRandomArbitrary(min, max) {
+		return Math.random() * (max - min) + min;
 	}
-}
-// Spint Slot
-var casinoAutoSpin;
-var casinoGoldAutoSpin;
-var casinoAgentJaneAutoSpin;
-document.querySelector('.button-spin-2').addEventListener('click', () => {
+
+	// Casiono game 2
+	var casinoGold1 = document.querySelector('#name1');
+	var casinoGold2 = document.querySelector('#name2');
+	var casinoGold3 = document.querySelector('#name3');
+	var casinoGold4 = document.querySelector('#name4');
 	if (casinoGold1 && casinoGold2 && casinoGold3 && casinoGold4) {
-		clearInterval(casinoGoldAutoSpin);
-		 gameSlotTwo()
-		//count = 3;
-		//mcasinoGold1.shuffle(9999);
-		//mcasinoGold2.shuffle(9999);
-		//mcasinoGold3.shuffle(9999);
-		//mcasinoGold4.shuffle(9999);
-		//setTimeout(() => mcasinoGold1.stop(), getRandomArbitrary(minTime, maxTime));
-		//setTimeout(() => mcasinoGold2.stop(), getRandomArbitrary(minTime, maxTime));
-		//setTimeout(() => mcasinoGold3.stop(), getRandomArbitrary(minTime, maxTime));
-		//setTimeout(() => mcasinoGold4.stop(), getRandomArbitrary(minTime, maxTime));
+		var mcasinoGold1 = new SlotMachine(casinoGold1, {
+			active: 0,
+			delay: 500
+		});
+		var mcasinoGold2 = new SlotMachine(casinoGold2, {
+			active: 1,
+			delay: 500
+		});
+		var mcasinoGold3 = new SlotMachine(casinoGold3, {
+			active: 2,
+			delay: 500
+		});
+		var mcasinoGold4 = new SlotMachine(casinoGold4, {
+			active: 3,
+			delay: 500
+		});
+
+		function gameSlotTwo() {
+			mcasinoGold1.shuffle(9999);
+			mcasinoGold2.shuffle(9999);
+			mcasinoGold3.shuffle(9999);
+			mcasinoGold4.shuffle(9999);
+			setTimeout(() => mcasinoGold1.stop(), getRandomArbitrary(minTime, maxTime));
+			setTimeout(() => mcasinoGold2.stop(), getRandomArbitrary(minTime, maxTime));
+			setTimeout(() => mcasinoGold3.stop(), getRandomArbitrary(minTime, maxTime));
+			setTimeout(() => mcasinoGold4.stop(), getRandomArbitrary(minTime, maxTime));
+		}
 	}
-})
+	// Spint Slot
+	var casinoAutoSpin;
+	var casinoGoldAutoSpin;
+	var casinoAgentJaneAutoSpin;
+	document.querySelector('.button-spin-2').addEventListener('click', () => {
+		if (casinoGold1 && casinoGold2 && casinoGold3 && casinoGold4) {
+			clearInterval(casinoGoldAutoSpin);
+			gameSlotTwo()
+			// count = 3;
+			// mcasinoGold1.shuffle(9999);
+			// mcasinoGold2.shuffle(9999);
+			// mcasinoGold3.shuffle(9999);
+			// mcasinoGold4.shuffle(9999);
+			// setTimeout(() => mcasinoGold1.stop(), getRandomArbitrary(minTime, maxTime));
+			// setTimeout(() => mcasinoGold2.stop(), getRandomArbitrary(minTime, maxTime));
+			// setTimeout(() => mcasinoGold3.stop(), getRandomArbitrary(minTime, maxTime));
+			// setTimeout(() => mcasinoGold4.stop(), getRandomArbitrary(minTime, maxTime));
+		}
+	})
+
+}
+
